011070889d88cdf0f36135bb689601dc3f6a7f8e
DarthBaluu <axel.lammertz@gmx.de>
2017-06-06 20:01:13 +0200

    Bestanden/Abgeschlossen

---
+++ sourcen/emain.c
@@ -0,0 +1,240 @@
+﻿#include "simuc.h"
+#include "io_treiber.h"
+
+// Die Verwendung der folgenden Zustandsnamen ist verbindlich
+typedef enum{Steht,StarteRunter,StarteRauf,Notaus,FahreRauf_MIT_OF,FahreRauf_OHNE_OF,FahreRunter_MIT_OF,FahreRunter_OHNE_OF} state_t;
+
+// Die folgenden Defines muessen sinnvoll genutzt werden
+#define BIT_T2		5
+#define BIT_T1		4
+#define BIT_ESR		3
+#define BIT_ESL		2
+
+#define BIT_M_Re	2
+#define BIT_M_Li	1
+#define BIT_M_An
+
+long int startpos;
+long int endpos;
+state_t the_state=Steht;
+// Hier die Steuerungsfunktion definieren
+// ...
+
+void steuerungsfunktion(int notaus, int t2, int esr, int esl, int* m_re, int* m_li, int* m_an, long int akt_pos, state_t* the_state){
+
+    switch(*the_state){
+    case Steht:
+        if (notaus==1){
+            *the_state=Notaus;
+            break;
+        }
+        else if(esr==1&&esl==0){
+            *the_state=StarteRauf;
+        }
+        else if (esr==0&&esl==1){
+            *the_state=StarteRunter;
+        }
+        break;
+    case StarteRauf:
+        if (notaus==1){
+            *the_state=Notaus;
+            break;
+        }
+        else if(akt_pos>=endpos){
+            *the_state=FahreRauf_OHNE_OF;
+        }
+        else if(akt_pos<endpos){
+            *the_state=FahreRauf_MIT_OF;
+        }
+        break;
+    case StarteRunter:
+        if (notaus==1){
+            *the_state=Notaus;
+            break;
+        }
+        else if(akt_pos>endpos){
+            *the_state=FahreRunter_MIT_OF;
+        }
+        else if (akt_pos<=endpos){
+            *the_state=FahreRunter_OHNE_OF;
+        }
+        break;
+    case Notaus:
+       if(t2==1&&notaus==0){
+           *the_state=Steht;
+       }
+        break;
+    case FahreRauf_MIT_OF:
+        if (notaus==1){
+            *the_state=Notaus;
+            break;
+        }
+        else if (akt_pos<endpos && akt_pos>startpos){
+            *the_state=Steht;
+        }
+        else if (esr==1){
+            *the_state=StarteRauf;
+        }
+        break;
+    case FahreRauf_OHNE_OF:
+        if (notaus==1){
+            *the_state=Notaus;
+            break;
+        }
+        else if (akt_pos<endpos || akt_pos>startpos){
+            *the_state=Steht;
+        }
+        else if(esr==1){
+            *the_state=StarteRauf;
+        }
+        break;
+    case FahreRunter_MIT_OF:
+        if (notaus==1){
+            *the_state=Notaus;
+            break;
+        }
+        else if(akt_pos>endpos && akt_pos<startpos){
+            *the_state=Steht;
+        }
+        else if(esl==1){
+            *the_state=StarteRunter;
+        }
+        break;
+    case FahreRunter_OHNE_OF:
+        if (notaus==1){
+            *the_state=Notaus;
+            break;
+        }
+        else if(akt_pos>endpos || akt_pos<startpos){
+            *the_state=Steht;
+        }
+        else if(esl==1){
+            *the_state=StarteRunter;
+        }
+        break;
+    }
+
+
+    switch(*the_state){
+    case Steht:
+        *m_li=0;
+        *m_re=0;
+        *m_an=0;
+        break;
+    case StarteRauf:
+        *m_li=1;
+        *m_re=0;
+        *m_an=1;
+        startpos=akt_pos;
+        endpos = (startpos-60000)%65535;
+        if(endpos<0){
+            endpos=endpos+65535;
+        }
+        break;
+    case StarteRunter:
+        *m_li=0;
+        *m_re=1;
+        *m_an=1;
+        startpos=akt_pos;
+        endpos = (startpos+60000)%65535;
+        break;
+    case Notaus:
+        *m_li=0;
+        *m_re=0;
+        *m_an=0;
+        break;
+    case FahreRauf_MIT_OF:
+        *m_li=1;
+        *m_re=0;
+        *m_an=1;
+        break;
+    case FahreRauf_OHNE_OF:
+        *m_li=1;
+        *m_re=0;
+        *m_an=1;
+        break;
+    case FahreRunter_MIT_OF:
+        *m_li=0;
+        *m_re=1;
+        *m_an=1;
+        break;
+    case FahreRunter_OHNE_OF:
+        *m_li=0;
+        *m_re=1;
+        *m_an=1;
+        break;
+    }
+}
+
+void emain(void* arg) 
+{
+	// Hier alle benoetigten Variablen deklarieren
+	// ...
+
+    unsigned short int wert=1;
+    unsigned short int *DigitalValue,temp;
+    DigitalValue = &wert;
+
+
+	INIT_BM_WITH_REGISTER_UI; // Hier unbedingt einen Break-Point setzen !!!
+
+	
+	// Hier die Treiberfunktionen aufrufen und testen (Aufgabe 1)
+	// ...
+
+  // io_out16(OUT1,wert);
+  // io_out16(DIR1,wert);
+   // Init(GlobalBoardHandle,0x89);
+
+/*
+    OutputByte(GlobalBoardHandle,3,5);
+    OutputByte(GlobalBoardHandle,2,6);
+    OutputByte(GlobalBoardHandle,1,255);
+    OutputByte(GlobalBoardHandle,0,255);
+/*
+        InputByte(GlobalBoardHandle,3,&wert);
+        InputByte(GlobalBoardHandle,2,&wert);
+        InputByte(GlobalBoardHandle,1,&wert);
+        InputByte(GlobalBoardHandle,0,&wert);
+
+*/
+
+	// Ab hier beginnt die Endlosschleife fuer den Automaten (Aufgabe 2)
+    Free(GlobalBoardHandle);
+    Init(GlobalBoardHandle,0x9A);
+
+	while(1) {
+
+		SYNC_SIM; 
+        unsigned short int portd, portc, portb, porta;
+		// Hier die Eingabesignale einlesen
+		// ...
+
+        InputByte(GlobalBoardHandle,2,&portc);
+        InputByte(GlobalBoardHandle,1,&portb);
+        InputByte(GlobalBoardHandle,0,&porta);
+        int i;
+        int notaus, t2, esr, esl, m_re, m_li, m_an;
+        long int akt_pos = 0;
+        esr=(portc>>3)&0x1;
+        esl=(portc>>2)&0x1;
+        t2=(portc>>5)&0x1;
+        notaus=(portc>>4)&0x1;
+        akt_pos = (portb<<8)|porta;
+		// Hier die Steuerungsfunktion aufrufen
+		// ...
+
+        steuerungsfunktion(notaus, t2, esr, esl, &m_re, &m_li, &m_an, akt_pos, &the_state);
+
+
+		// Hier die Ausgabesignale ausgeben
+		// ...
+        portd= m_re<<2|m_li<<1|m_an;
+        OutputByte(GlobalBoardHandle,3,portd);
+
+	} // while(1)..
+    Free(GlobalBoardHandle);
+	
+}
+
+  
---
+++ sourcen/io_teiber.c
@@ -0,0 +1,198 @@
+﻿#include "simuc.h"
+#include "io_treiber.h"
+
+typedef struct BHandle_Data {
+	BYTE Board_allocated;	// 1=allocated, 0=free
+	BYTE Port_A_Direction;	// 1=Output, 0=Input
+	BYTE Port_B_Direction;
+	BYTE Port_C_Direction;
+	BYTE Port_D_Direction;
+} BHandle;
+
+typedef BHandle* DSCB;
+BHandle BoardHandle_Data;
+DSCB GlobalBoardHandle = &BoardHandle_Data;
+
+
+
+BYTE Init(DSCB BoardHandle, unsigned long int Steuerwort) {
+    BYTE a,b,c,d;
+
+    if (BoardHandle->Board_allocated==0){
+        switch(Steuerwort){
+            case 0x80: a=1;b=1;c=1;d=1; break;  // 1=Output, 0=Input
+            case 0x81: a=1;b=1;c=1;d=0; break;
+            case 0x88: a=1;b=1;c=0;d=1; break;
+            case 0x89: a=1;b=1;c=0;d=0; break;
+            case 0x82: a=1;b=0;c=1;d=1; break;
+            case 0x83: a=1;b=0;c=1;d=0; break;
+            case 0x8A: a=1;b=0;c=0;d=1; break;
+            case 0x8B: a=1;b=0;c=0;d=0; break;
+            case 0x90: a=0;b=1;c=1;d=1; break;
+            case 0x91: a=0;b=1;c=1;d=0; break;
+            case 0x98: a=0;b=1;c=0;d=1; break;
+            case 0x99: a=0;b=1;c=0;d=0; break;
+            case 0x92: a=0;b=0;c=1;d=1; break;
+            case 0x93: a=0;b=0;c=1;d=0; break;
+            case 0x9A: a=0;b=0;c=0;d=1; break;
+            case 0x9B: a=0;b=0;c=0;d=0; break;
+        }
+        BoardHandle->Board_allocated=1;
+        if(a==1){io_out16(DIR0,0x00ff);}
+        if(b==1){io_out16(DIR0, (io_in16(DIR0)|0xff00));}
+        if(c==1){io_out16(DIR1,0x00ff);}
+        if(d==1){io_out16(DIR1, (io_in16(DIR1)|0xff00));}
+
+        BoardHandle->Port_A_Direction=a;
+        BoardHandle->Port_B_Direction=b;
+        BoardHandle->Port_C_Direction=c;
+        BoardHandle->Port_D_Direction=d;
+        /*
+        BoardHandle_Data.Port_A_Direction=a;
+        BoardHandle_Data.Port_B_Direction=b;
+        BoardHandle_Data.Port_C_Direction=c;
+        BoardHandle_Data.Port_D_Direction=d;
+        */
+        return 0;
+    }
+
+    else{return 1;}
+
+}
+
+BYTE InputByte(DSCB BoardHandle, BYTE Port, BYTE *DigitalValue) {
+    unsigned short int /*value,*/ status=0,port=0;
+    //int i, erg=0;
+    if(BoardHandle->Board_allocated==0){
+        if(Port == 0){
+                if(BoardHandle->Port_A_Direction==0){
+                    port = io_in16(IN0);
+                    port = port & 0xFF;
+                }
+                else{status=1;}
+        }
+
+        else if(Port == 1){
+                if(BoardHandle->Port_B_Direction==0){
+                    port = io_in16(IN0);
+                    port = port >> 8;
+                }
+                else{status=1;}
+        }
+
+
+        else if(Port == 2){
+                if(BoardHandle->Port_C_Direction==0){
+                    port = io_in16(IN1);
+                    port =  port & 0xFF;
+                }
+                else{status=1;}
+        }
+
+        else if(Port == 3){
+                if(BoardHandle->Port_D_Direction==0){
+                    port = io_in16(IN1);
+                    port = port >> 8;
+                }
+                else{status=1;}
+        }
+
+        else{status=2;}
+}else{status=1;}
+
+/*
+    for(i=0;i<8;i++){           // berechnen des endwerts(bin -> dez)
+        value = port >> i;
+        value = value & 0x01;
+
+        erg += (value*(1<<i)); // 1 um i nach links geshiftet = 2^i
+    }
+*/
+
+        *DigitalValue = port; // wert von erg der Referent zuweisen (Rückgabe)
+
+    return status;
+  }
+
+
+
+
+
+BYTE OutputByte(DSCB BoardHandle, BYTE Port, BYTE DigitalValue) {
+	
+    unsigned short int muster,akt,digval,port_aus,status=0;
+            digval = DigitalValue;
+            //A
+            //DigitalValue(dez) -> dig(bin);
+            //    int dezi = *DigitalValue;
+            //    int test;
+            //    int zaehler=0;
+            //    muster = 0x00;
+            //    while (dezi!=0){
+            //        test = dezi%2;
+            //        test << zaehler;
+            //        muster = muster|test;
+            //        zaehler++;
+            //        dezi = dezi/2;
+            //    }
+            //A
+    if(BoardHandle->Board_allocated==0){
+        if(Port == 0/*A*/){
+            if(BoardHandle->Port_A_Direction==1){
+                akt = io_in16(OUT0);
+                muster = digval & 0xFF;     // muster ist digval(Digital Value) mit sicherheit : 00000000xxxxxxxx
+                akt = akt & 0xFF00;         // akt = port mit musster xxxxxxxx00000000
+                port_aus = akt | muster;    // kombination von muster und akt
+                io_out16(OUT0,port_aus);    //Ausgabe auf OUT0
+            }
+            else{status=1;} //ungültiger BoardHandle
+         }
+
+        else if(Port == 1/*B*/){
+            if(BoardHandle->Port_B_Direction==1){
+                akt = io_in16(OUT0);
+                digval = digval<<8;                // Bits auf Porthöhe geshiftet
+                muster = digval & 0xFF00;   // muster ist digaval mit sicherheit : xxxxxxxx00000000
+                akt = akt & 0xFF;           // akt = port mit muster 00000000xxxxxxxx
+                port_aus = akt | muster;    // kombinieren
+                io_out16(OUT0,port_aus);    //Ausgabe auf OUT0
+            }
+            else{status=1;} //ungültiger BoardHandle
+        }
+
+
+        else if(Port == 2/*C*/){
+            if(BoardHandle->Port_C_Direction==1){
+                akt = io_in16(OUT1);        //Einlesen von OUT1
+                muster = digval & 0xFF;     // muster ist digval(Digital Value) mit sicherheit : 00000000xxxxxxxx
+                akt = akt & 0xFF00;         // akt = port mit musster xxxxxxxx00000000
+                port_aus = akt | muster;    // kombination von muster und akt
+                io_out16(OUT1,port_aus);    //Ausgabe auf OUT1
+            }
+            else{status=1;} //ungültiger BoardHandle
+        }
+
+        else if(Port == 3/*D*/){
+           if(BoardHandle->Port_D_Direction==1){
+                akt = io_in16(OUT1);        //Einlesen von OUT1
+                digval = digval<<8;             // Bits auf Porthöhe geshiftet
+                muster = digval & 0xFF00;   // muster ist digval mit sicherheit : xxxxxxxx00000000
+                akt = akt & 0xFF;           // akt = port mit muster 00000000xxxxxxxx
+                port_aus = akt | muster;    // kombinieren
+                io_out16(OUT1,port_aus);    //Ausgabe auf OUT1
+           }
+           else{status=1;} //ungültiger BoardHandle
+        }
+        else{status=2;} // ungültiger Port
+    }else{status=1;}
+
+        return status;
+}
+
+BYTE Free(DSCB BoardHandle) {
+
+    BoardHandle->Board_allocated=0;
+	
+    return 0;
+}
+
